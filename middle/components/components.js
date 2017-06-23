472ca3642731f6d74ae300d87d764c2e7c93b188
maqunboy <markexin@sina.com>
2017-06-23 15:35:03 +0800

    增加模块接口

--- middle/components/components.js
+++ middle/components/components.js
@@ -1,5 +1,5 @@
 /**
-*************************
+ *************************
  * auther：马群
  * QQ：2381930754
  * components相关API
@@ -12,19 +12,19 @@ var http = require("http"),
     connection = require('../config.js'),
     config = require('./content/config.js'),
     template = require('./content/template.js'),
-    fs=require("fs"),
+    fs = require("fs"),
     spawn = require('child_process').exec;
-    app = express();
+app = express();
 
 // 截取方法
-const SUB = (str, content, start , end, start2, end2 , bol) => {
-  var a = str.substring(start, end);
-  var b = str.substring(start2, end2);
-  if (bol) {
-    return `${a},${content}${b} `;
-  }else {
-      return `${a}${content}${b} `;
-  }
+const SUB = (str, content, start, end, start2, end2, bol) => {
+    var a = str.substring(start, end);
+    var b = str.substring(start2, end2);
+    if (bol) {
+        return `${a},${content}${b} `;
+    } else {
+        return `${a}${content}${b} `;
+    }
 };
 const createFolder = (to) => { //文件写入
     var sep = path.sep;
@@ -47,7 +47,7 @@ function md5(data) {
 
 
 // 拷贝文件接口
-app.post('/api/copyFile', function(req, res){
+app.post('/api/copyFile', function(req, res) {
     // 执行命令
     var ip = req.body.ip;
     var ssd = req.body.ssd;
@@ -58,190 +58,200 @@ app.post('/api/copyFile', function(req, res){
     var sys = req.body.sys;
     var endStr = "";
     // // 执行命令加载依赖
-    spawn(`cd /user/maqun/Desktop && cp -r trunk ${objname} && cd /user/maqun/Desktop/${objname}`,function (error, stdout, stderr) {
-            if (error == null) write();
+    spawn(`cd /user/maqun/Desktop && cp -r trunk ${objname} && cd /user/maqun/Desktop/${objname}`, function(error, stdout, stderr) {
+        if (error == null) write();
     });
     // 写入文件   process(buffer)
     var write = () => {
-      // 写入配置
-      fs.writeFile(`/user/maqun/Desktop/${objname}/config/index.js`, new Buffer(config.process(ip,ssd,root)),  (err, data) => {
-        if (err) {
-          throw err
-        }else {
-          // 写入文件
-          writeFileload();
-          return res.status(200).json({"type":true,"data":`项目创建成功，启动端口号为: ${ssd}`});
-        }
-      });
+        // 写入配置
+        fs.writeFile(`/user/maqun/Desktop/${objname}/config/index.js`, new Buffer(config.process(ip, ssd, root)), (err, data) => {
+            if (err) {
+                throw err
+            } else {
+                // 写入文件
+                writeFileload();
+                return res.status(200).json({ "type": true, "data": `项目创建成功，启动端口号为: ${ssd}` });
+            }
+        });
     };
 
     var writeFileload = () => {
 
-    // 创建文件   process(buffer)
-    fs.open(`/user/maqun/Desktop/${objname}/src/components/${name}.vue`,"w+",(err,fd) => {
-      if(err!==null){
-          console.error(err);
-          return;
-      }
-      var buf = new Buffer(template.data());
-      fs.write(fd,buf,0,buf.length,0);
-    });
+        // 创建文件   process(buffer)
+        fs.open(`/user/maqun/Desktop/${objname}/src/components/${name}.vue`, "w+", (err, fd) => {
+            if (err !== null) {
+                console.error(err);
+                return;
+            }
+            var buf = new Buffer(template.data());
+            fs.write(fd, buf, 0, buf.length, 0);
+        });
 
-    // 写入路由
-    fs.readFile(`/user/maqun/Desktop/${objname}/src/routes.js`,(err,buffer) => {
-      var str = buffer.toString();
-      var a = `import ${name} from 'components/${name}';`;
-      var b = `{
-      		path: '/${name}',
-              component: ${name}
-          }`;
+        // 写入路由
+        fs.readFile(`/user/maqun/Desktop/${objname}/src/routes.js`, (err, buffer) => {
+            var str = buffer.toString();
+            var a = `import ${name} from 'components/${name}';`;
+            var b = `{
+                path: '/${name}',
+                component: ${name}
+            }`;
+            endStr = SUB(str, b, 0, str.length - 3, str.length - 3, str.length, true);
+            endStr = SUB(endStr, a, 0, 0, 0, endStr.length, false);
 
-      endStr = SUB(str, b , 0 , str.length-3, str.length-3, str.length,true);
-      endStr = SUB(endStr,a ,0,0,0, endStr.length,false);
+            //写入路由信息
+            fs.writeFile(`/user/maqun/Desktop/${objname}/src/routes.js`, new Buffer(endStr), { flag: 'r+', encoding: 'utf8' }, (err, data) => {
+                if (err) throw err;
+            });
+        });
 
-      //写入路由信息
-      fs.writeFile(`/user/maqun/Desktop/${objname}/src/routes.js`, new Buffer(endStr), {flag: 'r+', encoding: 'utf8'},  (err, data) => {
-        if (err) throw err;
-      });
-    });
-
-  };
+    };
     // end
 });
 
 //  preview 项目
-app.post('/api/gopreview', function(req, res){
-  var objname = req.body.objname;
-  // var objname = "chonggou";
-  spawn(`cd /user/maqun/Desktop/${objname} && npm run dev`);
-  res.status(200).json({"type":true,"data":"启动成功！"});
+app.post('/api/gopreview', function(req, res) {
+    var objname = req.body.objname;
+    // var objname = "chonggou";
+    spawn(`cd /user/maqun/Desktop/${objname} && npm run dev`);
+    res.status(200).json({ "type": true, "data": "启动成功！" });
 })
 
 
 // 新增业务系统
-app.post('/api/newSystem', function(req, res){
+app.post('/api/newSystem', function(req, res) {
     var name = req.body.name;
     var person = req.body.person;
     var sign = md5(name);
-    connection.query("select id from "+connection.dev.call+".comp_systerm where id = '"+sign+"'", function(err, result) {
+    connection.query("select id from " + connection.dev.call + ".comp_systerm where id = '" + sign + "'", function(err, result) {
         if (result.length > 0) {
             // 数据存在
-            return res.status(200).json({"type":false,"data":"创建失败！系统名称重复"});
-        }else{
-            connection.query("INSERT INTO "+connection.dev.call+".comp_systerm (id, name, last_person) VALUES ('"+sign+"', '"+name+"','"+person+"')", function(err, result) {
+            return res.status(200).json({ "type": false, "data": "创建失败！系统名称重复" });
+        } else {
+            connection.query("INSERT INTO " + connection.dev.call + ".comp_systerm (id, name, last_person) VALUES ('" + sign + "', '" + name + "','" + person + "')", function(err, result) {
                 if (err) throw err;
-                else res.status(200).json({"type":true,"data":"系统创建成功！"});
+                else res.status(200).json({ "type": true, "data": "系统创建成功！" });
             })
         }
     })
 })
 
 // 查询业务系统列表
-app.post('/api/getnewSystem', function(req, res){
-    connection.query("select * from "+connection.dev.call+".comp_systerm", function(err, result) {
+app.post('/api/getnewSystem', function(req, res) {
+    connection.query("select * from " + connection.dev.call + ".comp_systerm", function(err, result) {
         if (err) throw err;
-        else res.status(200).json({"type":true,"data":result});
+        else res.status(200).json({ "type": true, "data": result });
     })
 })
 
 // 删除业务系统
-app.post('/api/delnewSystem', function(req, res){
+app.post('/api/delnewSystem', function(req, res) {
     var id = req.body.id;
-    connection.query("DELETE FROM "+connection.dev.call+".comp_systerm WHERE id='"+id+"'", function(err, result) {
+    connection.query("DELETE FROM " + connection.dev.call + ".comp_systerm WHERE id='" + id + "'", function(err, result) {
         if (err) throw err;
-        else res.status(200).json({"type":true,"data":"系统删除成功！"});
+        else res.status(200).json({ "type": true, "data": "系统删除成功！" });
     })
 })
 
 // 系统添加区块
-app.post('/api/addblock', function(req, res){
+app.post('/api/addblock', function(req, res) {
     var name = req.body.name;
     var html = req.body.html_code;
     var css = req.body.css_code;
     var title = req.body.title;
     var id = req.body.id;
     var sign = Date.parse(new Date());
-    var str = "UPDATE "+connection.dev.call+".block SET title='"+title+"',html_code='"+html+"',css_code='"+css+"',name='"+name+"' WHERE id='"+id+"'";
+    var str = "UPDATE " + connection.dev.call + ".block SET title='" + title + "',html_code='" + html + "',css_code='" + css + "',name='" + name + "' WHERE id='" + id + "'";
     if (id == "") {
-        str = "INSERT INTO "+connection.dev.call+".block (id, name,html_code,css_code,title) VALUES ('"+sign+"', '"+name+"','"+html+"','"+css+"','"+title+"')";
+        str = "INSERT INTO " + connection.dev.call + ".block (id, name,html_code,css_code,title) VALUES ('" + sign + "', '" + name + "','" + html + "','" + css + "','" + title + "')";
     }
     connection.query(str, function(err, result) {
         if (err) throw err;
-        else res.status(200).json({"type":true,"data":"区块添加成功！"});
+        else res.status(200).json({ "type": true, "data": "区块添加成功！" });
     })
 })
 
 // 系统区块查询
-app.post('/api/searchblock', function(req, res){
+app.post('/api/searchblock', function(req, res) {
     var id = req.body.id;
-    connection.query("SELECT id, name,cast(html_code as char) as html_code,cast(css_code as char) as css_code,title FROM "+connection.dev.call+".block WHERE id = '"+id+"' ", function(err, result) {
+    connection.query("SELECT id, name,cast(html_code as char) as html_code,cast(css_code as char) as css_code,title FROM " + connection.dev.call + ".block WHERE id = '" + id + "' ", function(err, result) {
         if (err) throw err;
-        else res.status(200).json({"type":true,"data":result});
+        else res.status(200).json({ "type": true, "data": result });
     })
 })
 
 // 删除区块
-app.post('/api/delblock', function(req, res){
+app.post('/api/delblock', function(req, res) {
     var id = req.body.id;
-    connection.query("DELETE FROM "+connection.dev.call+".block WHERE id='"+id+"'", function(err, result) {
+    connection.query("DELETE FROM " + connection.dev.call + ".block WHERE id='" + id + "'", function(err, result) {
         if (err) throw err;
-        else res.status(200).json({"type":true,"data":"删除成功！"});
+        else res.status(200).json({ "type": true, "data": "删除成功！" });
     })
 })
 
 // 区块下拉查询
-app.post('/api/blockListDrop', function(req, res){
-    connection.query("select id,title from "+connection.dev.call+".block", function(err, res0) {
+app.post('/api/blockListDrop', function(req, res) {
+    connection.query("select id,title from " + connection.dev.call + ".block", function(err, res0) {
         if (err) throw err;
-        else return res.status(200).json({"type":"success","data":res0});
+        else return res.status(200).json({ "type": "success", "data": res0 });
     })
 })
 
 // 区块列表
-app.post('/api/blockList', function(req, res){
+app.post('/api/blockList', function(req, res) {
     var searchValue = req.body.search;
-    var pageNum = (req.body.current -1)*10;
+    var pageNum = (req.body.current - 1) * 10;
     var current = req.body.current
     var pageTotal = '';
-    var str = "select id,name,title from "+connection.dev.call+".block where title like '%"+searchValue+"%'";
+    var str = "select id,name,title from " + connection.dev.call + ".block where title like '%" + searchValue + "%'";
     connection.query(str, function(err, result) {
         if (err) {
-                throw err;
-                return res.status(200).json({"type":"error","data":"服务端报错！"});
-         }else{
-                pageTotal = result.length;
-                str += "limit "+pageNum+",10";
-                connection.query(str, function(err, res0) {
-                    if (err) throw err;
-                    else return res.status(200).json({"type":"error","data":{
-                        data:res0,
-                        total:pageTotal,
-                        current:current
-                    }});
-                })
-         }
+            throw err;
+            return res.status(200).json({ "type": "error", "data": "服务端报错！" });
+        } else {
+            pageTotal = result.length;
+            str += "limit " + pageNum + ",10";
+            connection.query(str, function(err, res0) {
+                if (err) throw err;
+                else return res.status(200).json({
+                    "type": "error",
+                    "data": {
+                        data: res0,
+                        total: pageTotal,
+                        current: current
+                    }
+                });
+            })
+        }
     })
 })
 
 
-// // 模块新增
-// app.post('/api/templateAdd', function(req, res){
-//     var html = req.body.html.replace(/[\\"']/g,'\\$&');
-//     var title = req.body.title;
-//     var author = req.body.author;
-//     var css = req.body.css;
-//     var id = req.body.id;
-//     var sign = Date.parse(new Date());
-//     var str = "INSERT INTO "+connection.dev.call+".template (id, title,css_code,html_code,author) VALUES ('"+sign+"', '"+title+"','"+css+"','"+html+"','"+author+"')";
-//     // 区分新增编辑
-//     if (id) {
-//         str = "UPDATE "+connection.dev.call+".template SET title = '"+title+"',css_code = '"+css+"',html_code = '"+html+"',author = '"+author+"' WHERE id ='"+id+"'";
-//     }
-//     connection.query(str, function(err, result) {
-//         if (err) throw err;
-//         else return res.status(200).json({"type":"success","data":"模块更新成功！"})
-//     })
-// })
+// 模块新增
+app.post('/api/templateAdd', function(req, res) {
+    var html = req.body.html.replace(/[\\"']/g, '\\$&');
+    var title = req.body.title;
+    var author = req.body.author;
+    var html2 = req.body.html2;
+    var sign = Date.parse(new Date());
+    var str = "INSERT INTO " + connection.dev.call + ".template (id, title,template_code,html_code,author) VALUES ('" + sign + "', '" + title + "','" + html2 + "','" + html + "','" + author + "')";
+    connection.query(str, function(err, result) {
+        if (err) throw err;
+        else return res.status(200).json({ "type": "success", "data": "模块更新成功！" })
+    })
+});
+
+//模块列表下拉列表
+app.post('/api/templateListDrop', function(req, res) {
+    connection.query("select id, title,cast(template_code as char) as template_code,cast(html_code as char) as html_code,author from " + connection.dev.call + ".template", function(err, result) {
+        if (err) {
+            throw err;
+            return res.status(200).json({ "type": "error", "data": "服务端报错！" });
+        } else {
+            return res.status(200).json({ data: result });
+        }
+    })
+});
+
 //
 //
 // // 模块详情
@@ -263,17 +273,6 @@ app.post('/api/blockList', function(req, res){
 //     })
 // })
 //
-// //模块列表下拉列表
-// app.post('/api/templateListDrop', function(req, res){
-//     connection.query("select id, title,cast(css_code as char) as css_code,cast(html_code as char) as html_code,author from "+connection.dev.call+".template", function(err, result) {
-//         if (err) {
-//                 throw err;
-//                 return res.status(200).json({"type":"error","data":"服务端报错！"});
-//          }else {
-//                return  res.status(200).json({data:result});
-//         }
-//     })
-// })
 
 
 // 模块列表页查询
@@ -346,4 +345,4 @@ app.post('/api/blockList', function(req, res){
 //     })
 // })
 
- module.exports = app;
+module.exports = app;
